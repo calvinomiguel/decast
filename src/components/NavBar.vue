53300cac464ac39ab3feecb38717649444dd6724
calvinomiguel <31018737+calvinomiguel@users.noreply.github.com>
2020-11-16 20:19:05 +0100

    Config Tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav id="nav-bar">
+    <div class="nav-wrapper">
+      <a href="">
+        <img
+          src="https://assets.materialup.com/uploads/9c5966ae-16b1-4850-ac09-1cbaa1836f90/attachment.png"
+          alt="decast logo"
+        />
+        <ul>
+          <li><a class="font-mono" href="">Dashboard</a></li>
+          <li><a href="">Files</a></li>
+          <li><a href="">Components</a></li>
+        </ul>
+      </a>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+<style scoped>
+
+</style>
