7f43f301217286dc82f7f6d0e58c428ff414e5b1
Calvino Miguel <c.antoniomiguel@gmail.com>
2020-12-20 19:25:10 +0100

    Fetch Client POST request in Server

--- api/app.js
+++ api/app.js
@@ -1,57 +1,33 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const bodyParser = require('body-parser');
+const router = express.Router();
 const upload = multer({
     dest: "./uploads"
 });
 const cors = require('cors');
 app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
 //const uploadsFolder = __dirname + "/uploads/";
 const port = process.env.PORT || 3060;
 //const fs = require("fs");
 //const path = require("path");
 //const { res } = require("express");
 //const dirPath = path.join(__dirname, "uploads");
-app.use(express.json());
-app.use(express.urlencoded());
-
+/*
 //Set files in upload folder
 app.post("/files", upload.array("sketch-files"), (req, res) => {
     console.log(req.files);
     req.send(req.files);
 });
-
-const files = [{
-        id: 1,
-        name: "decast.sketch"
-    },
-    {
-        id: 2,
-        name: "lbry.sketch"
-    },
-    {
-        id: 3,
-        name: "bitchute.sketch"
-    },
-    {
-        id: 4,
-        name: "parler.sketch"
-    }
-];
-/*
-app.post("/uploads", (req, res) => {
-    const file = {
-        "id": files.length + 1,
-        "name": req.body.name
-    };
-    files.push(file);
-    res.send(file);
-});
-*/
-app.post("/uploads", (req, res) => {
-    res.send(req.body);
-});
-
 //Get files in uploads folder
 /*app.get('/api/uploads', (req, res) => {
     fs.readdir(dirPath, (err, files) => {
@@ -66,6 +42,16 @@ app.post("/uploads", (req, res) => {
 app.get("/", (req, res) => {
     res.send("<html><head><title>API Server</title></head><body><h1>Hello World!</h1></body></html>");
 });
+
+//Handle POST Request from Client Form
+router.post("/uploads", (req, res) => {
+    console.log(req.body);
+});
+
+
+
+// add router in the Express app.
+app.use("/", router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
