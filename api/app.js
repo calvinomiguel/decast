4628a94fcafb27e3f30ad10ffec295a6c3ceddfb
Calvino Miguel <c.antoniomiguel@gmail.com>
2020-12-22 10:18:48 +0100

    Unzip files

--- api/app.js
+++ api/app.js
@@ -4,19 +4,21 @@ const multer = require("multer");
 const bodyParser = require('body-parser');
 const router = express.Router();
 const fs = require('fs');
+const decompress = require('decompress');
+const port = process.env.PORT || 3060;
 const upload = multer({
     dest: "./uploads/"
 });
 const cors = require('cors');
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 );
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
-const port = process.env.PORT || 3060;
 
 //Handle POST Request from Client Form
 router.post("/uploads", upload.array("files"), (req, res) => {
@@ -29,7 +31,7 @@ router.post("/uploads", upload.array("files"), (req, res) => {
             oName: file.originalname.replace(".sketch", ".zip"),
             tmpName: file.filename
         });
-    })
+    });
 
     //Function for changing the name of the uploaded file to original name
     function changeFileName() {
@@ -44,11 +46,23 @@ router.post("/uploads", upload.array("files"), (req, res) => {
             console.log(file);
         });
     }
-    console.log(req.files);
-    res.sendStatus(200);
-    changeFileName();
-});
 
+    function unzipFiles() {
+        fs.readdirSync(`${__dirname}/uploads`).forEach(file => {
+            (async () => {
+                try {
+                    await decompress(`${__dirname}/uploads/${file}`, `${__dirname}/uploads/`);
+                } catch (err) {
+                    console.log(err);
+                }
+            })();
+        });
+    }
+    console.log(req.files);
+    changeFileName();
+    unzipFiles();
+    res.sendStatus(200);
+});
 
 // add router in the Express app.
 app.use("/", router);
@@ -60,24 +74,3 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.send("<html><head><title>API Server</title></head><body><h1>Hello World!</h1></body></html>");
 });
-
-//const fs = require("fs");
-//const path = require("path");
-//const { res } = require("express");
-//const dirPath = path.join(__dirname, "uploads");
-/*
-//Set files in upload folder
-app.post("/files", upload.array("sketch-files"), (req, res) => {
-    console.log(req.files);
-    req.send(req.files);
-});
-//Get files in uploads folder
-/*app.get('/api/uploads', (req, res) => {
-    fs.readdir(dirPath, (err, files) => {
-        if (err) {
-            return console.log('Unable to scan directory:' + err);
-        } 
-        res.send(files);
-    });
-});
-*/
