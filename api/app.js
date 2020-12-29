072d79bf58565f990eb1f9b00c3f0b383dfdabdb
Calvino Miguel <c.antoniomiguel@gmail.com>
2020-12-30 00:08:15 +0100

    Fix async functions

--- api/app.js
+++ api/app.js
@@ -393,177 +393,6 @@ async function createObjSchema() {
     return filesContent;
 }
 
-/*
-function createObjSchema() {
-    let PATH__L1 = [];
-
-    //Get all folders inside of uploads folder
-    fs.readdirSync(dir).forEach(file => {
-        //Save folder names into array
-        if (!file.includes('.DS_Store') && !file.includes(".zip")) {
-            PATH__L1.push({
-                name: file,
-                path: dir + "/" + file
-            });
-        }
-    });
-
-    PATH__L1.forEach(folder => {
-        folder.json = [];
-        folder.dir = {
-            pages: {
-                files: "",
-                path: ""
-            },
-            images: {
-                files: "",
-                path: false
-            },
-            previews: {
-                files: "",
-                path: ""
-            },
-            textPreviews: {
-                files: "",
-                path: false
-            }
-        };
-        console.log("WHERE THE ERROR HAPPENS");
-
-
-        console.log(folder.path);
-        fs.readdirSync(folder.path).forEach(file => {
-            if (file.includes("pages")) {
-                folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
-            }
-            if (file.includes("images")) {
-                folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
-            }
-            if (file.includes("previews")) {
-                folder.dir.previews.path = dir + "/" + folder.name + "/" + file;
-            }
-            if (file.includes("text-previews")) {
-                folder.dir.textPreviews.path = dir + "/" + folder.name + "/" + file;
-            }
-            if (file.includes("document") || file.includes("meta") || file.includes("user")) {
-                folder.json.push({
-                    name: file,
-                    path: folder.path + "/" + file,
-                    content: ""
-                });
-            }
-        });
-
-    });
-
-    PATH__L1.forEach(folder => {
-        let pages = folder.dir.pages;
-        let previews = folder.dir.previews;
-        let textPreviews = folder.dir.textPreviews;
-        let images = folder.dir.images;
-        pages.files = [];
-        previews.files = [];
-        textPreviews.files = [];
-        images.files = [];
-
-        console.log("Next error here!!!");
-        console.log(pages.path)
-        if (!pages.path.includes('.zip')) {
-            fs.readdirSync(pages.path).forEach(file => {
-                pages.files.push({
-                    name: file,
-                    path: pages.path + "/" + file,
-                    content: ""
-                });
-            });
-
-            fs.readdirSync(previews.path).forEach(file => {
-                previews.files.push({
-                    name: file,
-                    path: previews.path + "/" + file,
-                    content: ""
-                });
-            });
-
-            if (textPreviews.path != false) {
-                fs.readdirSync(textPreviews.path).forEach(file => {
-                    textPreviews.files.push({
-                        name: file,
-                        path: previews.path + "/" + file,
-                        content: ""
-                    });
-                });
-            }
-
-            if (images.path != false) {
-                fs.readdirSync(images.path).forEach(file => {
-                    images.files.push({
-                        name: file,
-                        path: previews.path + "/" + file,
-                        content: ""
-                    });
-                });
-            }
-        }
-
-    });
-
-    //META, DOC & USER JSON FILES
-    PATH__L1.forEach(folder => {
-        let jsonFiles = folder.json;
-
-        jsonFiles.forEach(file => {
-            let data = fs.readFileSync(file.path, "utf8")
-            file.content = JSON.parse(data);
-        });
-    });
-
-    //PAGE FILES
-    PATH__L1.forEach(folder => {
-        let pageFiles = folder.dir.pages.files;
-
-        pageFiles.forEach(file => {
-            let data = fs.readFileSync(file.path, "utf8")
-            file.content = JSON.parse(data);
-        });
-    });
-
-    //IMAGES FILES
-    PATH__L1.forEach(folder => {
-        let imageFiles = folder.dir.images.files;
-
-        imageFiles.forEach(file => {
-            let data = fs.readFileSync(file.path, "base64")
-            file.content = data;
-        });
-    });
-
-    //Previews FILES
-    PATH__L1.forEach(folder => {
-        let previewFiles = folder.dir.previews.files;
-
-        previewFiles.forEach(file => {
-            let data = fs.readFileSync(file.path, "base64")
-            file.content = data;
-        });
-    });
-
-    //Text Previews FILES
-    PATH__L1.forEach(folder => {
-        let textPreviewFiles = folder.dir.textPreviews.files;
-
-        textPreviewFiles.forEach(file => {
-            if (folder.dir.textPreviews.path != false) {
-                let encoding = file.name.includes('.pdf') ? "base64" : "utf8";
-                let data = fs.readFileSync(file.path, "utf8")
-                file.content = file.name.includes('.pdf') ? data : JSON.parse(data);
-            }
-        });
-    });
-
-    return PATH__L1;
-}*/
-
 async function getPages() {
     let pages = [];
     let objSchema = await createObjSchema();
@@ -573,8 +402,8 @@ async function getPages() {
     return pages;
 }
 
-function getDocFiles() {
-    let schema = createObjSchema();
+async function getDocFiles() {
+    let schema = await createObjSchema();
     let docFiles = [];
     schema.forEach(obj => {
         let jsonList = obj.json;
@@ -628,9 +457,9 @@ async function getSymbolInstances() {
     return symbolInstances;
 }
 
-function getForeignSymbols() {
-    let docs = getDocFiles();
+async function getForeignSymbols() {
     let foreignSymbols = [];
+    let docs = await getDocFiles();
     docs.forEach(doc => {
         let symbolsList = doc.foreignSymbols;
         if (symbolsList != undefined && symbolsList != null && symbolsList.length > 0) {
@@ -700,7 +529,7 @@ async function getMasterSymbols() {
 
     deliverable.count = symbolsCount;
     deliverable.symbols = masterSymbols;
-    console.log(deliverable)
+    getForeignSymbols(getDocFiles());
     return deliverable;
 }
 
