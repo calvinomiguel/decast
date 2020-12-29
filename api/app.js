ed3d58d2033998d192815b4428a35881e2a4db40
Calvino Miguel <c.antoniomiguel@gmail.com>
2020-12-29 16:48:51 +0100

    Refactored objschema

--- api/app.js
+++ api/app.js
@@ -180,6 +180,7 @@ async function processPostRequest(request) {
 function getFolders() {
     return new Promise((resolve, reject) => {
         let folders = [];
+        let controller = false;
         //Get all folders inside of uploads folder
         fs.readdirSync(dir).forEach(file => {
             //Save folder names and paths into folders array
@@ -190,7 +191,8 @@ function getFolders() {
                 });
             }
         });
-        if (folders.length > 0) {
+        controller = true;
+        if (folders.length > 0 && controller == true) {
             resolve(folders);
         } else {
             reject("Something went wrong while getting the folders names and paths.");
@@ -200,6 +202,7 @@ function getFolders() {
 
 function setObjStructure(folders) {
     return new Promise((resolve, reject) => {
+        let controller = false;
         folders.forEach(folder => {
             folder.json = [];
             folder.dir = {
@@ -220,13 +223,179 @@ function setObjStructure(folders) {
                     path: false
                 }
             };
+
+            fs.readdirSync(folder.path).forEach(file => {
+                if (!file.includes(".DS_Store")) {
+                    if (file.includes("pages")) {
+                        folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
+                    }
+                    if (file.includes("images")) {
+                        folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
+                    }
+                    if (file.includes("previews")) {
+                        folder.dir.previews.path = dir + "/" + folder.name + "/" + file;
+                    }
+                    if (file.includes("text-previews")) {
+                        folder.dir.textPreviews.path = dir + "/" + folder.name + "/" + file;
+                    }
+                    if (file.includes("document") || file.includes("meta") || file.includes("user")) {
+                        folder.json.push({
+                            name: file,
+                            path: folder.path + "/" + file,
+                            content: ""
+                        });
+                    }
+                }
+
+            });
         });
+        controller = true;
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("Something went wrong while setting the folders structure.");
+        }
     });
 }
 
+function setObjSubFolders(folders) {
+    return new Promise((resolve, reject) => {
+        let controller = false;
+        folders.forEach(folder => {
+            let pages = folder.dir.pages;
+            let previews = folder.dir.previews;
+            let textPreviews = folder.dir.textPreviews;
+            let images = folder.dir.images;
+            pages.files = [];
+            previews.files = [];
+            textPreviews.files = [];
+            images.files = [];
+
+            if (!pages.path.includes('.zip') && !pages.path.includes('.DS_Store')) {
+                fs.readdirSync(pages.path).forEach(file => {
+                    pages.files.push({
+                        name: file,
+                        path: pages.path + "/" + file,
+                        content: ""
+                    });
+                });
+
+                fs.readdirSync(previews.path).forEach(file => {
+                    previews.files.push({
+                        name: file,
+                        path: previews.path + "/" + file,
+                        content: ""
+                    });
+                });
+
+                if (textPreviews.path != false) {
+                    fs.readdirSync(textPreviews.path).forEach(file => {
+                        textPreviews.files.push({
+                            name: file,
+                            path: previews.path + "/" + file,
+                            content: ""
+                        });
+                    });
+                }
+
+                if (images.path != false) {
+                    fs.readdirSync(images.path).forEach(file => {
+                        images.files.push({
+                            name: file,
+                            path: previews.path + "/" + file,
+                            content: ""
+                        });
+                    });
+                }
+            }
+            controller = true;
+        });
+
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("");
+        }
+    });
+}
+
+function getFilesContent(folders) {
+    return new Promise((resolve, reject) => {
+        let controller;
+        //META, DOC & USER JSON FILES
+        folders.forEach(folder => {
+            let jsonFiles = folder.json;
+
+            jsonFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "utf8")
+                file.content = JSON.parse(data);
+            });
+        });
+
+        //PAGE FILES
+        folders.forEach(folder => {
+            let pageFiles = folder.dir.pages.files;
+
+            pageFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "utf8")
+                file.content = JSON.parse(data);
+            });
+        });
+
+        //IMAGES FILES
+        folders.forEach(folder => {
+            let imageFiles = folder.dir.images.files;
+
+            imageFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "base64")
+                file.content = data;
+            });
+        });
+
+        //Previews FILES
+        folders.forEach(folder => {
+            let previewFiles = folder.dir.previews.files;
+
+            previewFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "base64")
+                file.content = data;
+            });
+        });
+
+        //Text Previews FILES
+        folders.forEach(folder => {
+            let textPreviewFiles = folder.dir.textPreviews.files;
+
+            textPreviewFiles.forEach(file => {
+                if (folder.dir.textPreviews.path != false) {
+                    let encoding = file.name.includes('.pdf') ? "base64" : "utf8";
+                    let data = fs.readFileSync(file.path, "utf8")
+                    file.content = file.name.includes('.pdf') ? data : JSON.parse(data);
+                }
+            });
+        });
+        controller = true;
+
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("Something went wrong while getting json content.");
+        }
+    });
+}
+
+async function createObjSchema() {
+    let folders = await getFolders();
+    let objStructure = await setObjStructure(folders);
+    let objSubFolders = await setObjSubFolders(objStructure);
+    let filesContent = await getFilesContent(objSubFolders);
+
+    return filesContent;
+}
+
+/*
 function createObjSchema() {
     let PATH__L1 = [];
-    let sketchFiles = [];
 
     //Get all folders inside of uploads folder
     fs.readdirSync(dir).forEach(file => {
@@ -393,11 +562,12 @@ function createObjSchema() {
     });
 
     return PATH__L1;
-}
+}*/
 
-function getPages() {
+async function getPages() {
     let pages = [];
-    createObjSchema().forEach(obj => {
+    let objSchema = await createObjSchema();
+    objSchema.forEach(obj => {
         pages.push(obj.dir.pages);
     })
     return pages;
@@ -417,11 +587,12 @@ function getDocFiles() {
     return docFiles;
 }
 
-function getPageFiles() {
+async function getPageFiles() {
     let filesGroups = [];
     let files = [];
     let deliverable = [];
-    getPages().forEach(obj => {
+    let pages = await getPages();
+    pages.forEach(obj => {
         filesGroups.push(obj);
     });
     filesGroups.forEach(group => {
@@ -436,9 +607,10 @@ function getPageFiles() {
     return deliverable;
 }
 
-function getSymbolInstances() {
+async function getSymbolInstances() {
     let symbolInstances = [];
-    getPageFiles().forEach(file => {
+    let pagesFiles = await getPageFiles();
+    pagesFiles.forEach(file => {
         let fileLayers = file.content.layers;
 
         fileLayers.forEach(fileLayer => {
@@ -488,12 +660,12 @@ function getForeignSymbols() {
 }
 
 
-function getMasterSymbols() {
+async function getMasterSymbols() {
     let masterSymbols = [];
     let symbolsCount = 0;
     let deliverable = {};
-    let objSchema = createObjSchema();
-
+    let objSchema = await createObjSchema();
+    let symbolInstances = await getSymbolInstances();
     //Get all Master Symbols
     objSchema.forEach(folder => {
         let pages = folder.dir.pages.files;
@@ -519,7 +691,7 @@ function getMasterSymbols() {
 
     //Check if if symbol is used
     masterSymbols.forEach(symbol => {
-        getSymbolInstances().forEach(layer => {
+        symbolInstances.forEach(layer => {
             if (layer._class == "symbolInstance" && layer.symbolID == symbol.symbolID) {
                 symbol.used = true;
             }
@@ -528,6 +700,7 @@ function getMasterSymbols() {
 
     deliverable.count = symbolsCount;
     deliverable.symbols = masterSymbols;
+    console.log(deliverable)
     return deliverable;
 }
 
@@ -538,8 +711,8 @@ router.post("/uploads", upload.array("files"), (req, res) => {
 });
 
 //Handle GET Request from Client Form
-router.get("/dashboard", (req, res) => {
-    res.status(200).send(getMasterSymbols());
+router.get("/dashboard", async (req, res) => {
+    res.status(200).send(await getMasterSymbols());
 });
 
 //Add router in the Express app.
--- api/schema.js
+++ api/schema.js
@@ -1,18 +1,206 @@
-let obj = {
-    _class: "MSImmutableForeignSymbol",
-    library: {
-        id: "B3C43A25-68DE-45E7-824D-EC02C36C26EC",
-        name: "decast"
-    },
-    originalMaster: {
-        id: "7ACE4064-E69D-48BA-B98E-F71B0F195238",
-        _class: "symbolMaster",
-        name: "btn-primary",
-    },
-    symbolMaster: {
-        id: "7ACE4064-E69D-48BA-B98E-F71B0F195238",
-        _class: "symbolMaster",
-        name: "btn-primary",
-    }
+//CREATING OBJ SCHEMA
+function getFolders() {
+    return new Promise((resolve, reject) => {
+        let folders = [];
+        let controller = false;
+        //Get all folders inside of uploads folder
+        fs.readdirSync(dir).forEach(file => {
+            //Save folder names and paths into folders array
+            if (!file.includes('.DS_Store') && !file.includes('.zip')) {
+                folders.push({
+                    name: file,
+                    path: dir + "/" + file
+                });
+            }
+        });
+        controller = true;
+        if (folders.length > 0 && controller == true) {
+            resolve(folders);
+        } else {
+            reject("Something went wrong while getting the folders names and paths.");
+        }
+    });
+}
 
-};
+function setObjStructure(folders) {
+    return new Promise((resolve, reject) => {
+        let controller = false;
+        folders.forEach(folder => {
+            folder.json = [];
+            folder.dir = {
+                pages: {
+                    files: "",
+                    path: ""
+                },
+                images: {
+                    files: "",
+                    path: false
+                },
+                previews: {
+                    files: "",
+                    path: ""
+                },
+                textPreviews: {
+                    files: "",
+                    path: false
+                }
+            };
+        });
+        fs.readdirSync(folder.path).forEach(file => {
+            if (!file.includes(".DS_Store")) {
+                if (file.includes("pages")) {
+                    folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
+                }
+                if (file.includes("images")) {
+                    folder.dir.pages.path = dir + "/" + folder.name + "/" + file;
+                }
+                if (file.includes("previews")) {
+                    folder.dir.previews.path = dir + "/" + folder.name + "/" + file;
+                }
+                if (file.includes("text-previews")) {
+                    folder.dir.textPreviews.path = dir + "/" + folder.name + "/" + file;
+                }
+                if (file.includes("document") || file.includes("meta") || file.includes("user")) {
+                    folder.json.push({
+                        name: file,
+                        path: folder.path + "/" + file,
+                        content: ""
+                    });
+                }
+            }
+
+        });
+        controller = true;
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("Something went wrong while setting the folders structure.");
+        }
+    });
+}
+
+function setObjSubFolders(folders) {
+    return new Promise((resolve, reject) => {
+        folders.forEach(folder => {
+            let controller = false;
+            let pages = folder.dir.pages;
+            let previews = folder.dir.previews;
+            let textPreviews = folder.dir.textPreviews;
+            let images = folder.dir.images;
+            pages.files = [];
+            previews.files = [];
+            textPreviews.files = [];
+            images.files = [];
+
+            if (!pages.path.includes('.zip') && !pages.path.includes('.DS_Store')) {
+                fs.readdirSync(pages.path).forEach(file => {
+                    pages.files.push({
+                        name: file,
+                        path: pages.path + "/" + file,
+                        content: ""
+                    });
+                });
+
+                fs.readdirSync(previews.path).forEach(file => {
+                    previews.files.push({
+                        name: file,
+                        path: previews.path + "/" + file,
+                        content: ""
+                    });
+                });
+
+                if (textPreviews.path != false) {
+                    fs.readdirSync(textPreviews.path).forEach(file => {
+                        textPreviews.files.push({
+                            name: file,
+                            path: previews.path + "/" + file,
+                            content: ""
+                        });
+                    });
+                }
+
+                if (images.path != false) {
+                    fs.readdirSync(images.path).forEach(file => {
+                        images.files.push({
+                            name: file,
+                            path: previews.path + "/" + file,
+                            content: ""
+                        });
+                    });
+                }
+            }
+            controller = true;
+        });
+
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("");
+        }
+    });
+}
+
+function getFilesContent(folders) {
+    return new Promise((resolve, reject) => {
+        let controller;
+        //META, DOC & USER JSON FILES
+        folders.forEach(folder => {
+            let jsonFiles = folder.json;
+
+            jsonFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "utf8")
+                file.content = JSON.parse(data);
+            });
+        });
+
+        //PAGE FILES
+        folders.forEach(folder => {
+            let pageFiles = folder.dir.pages.files;
+
+            pageFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "utf8")
+                file.content = JSON.parse(data);
+            });
+        });
+
+        //IMAGES FILES
+        folders.forEach(folder => {
+            let imageFiles = folder.dir.images.files;
+
+            imageFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "base64")
+                file.content = data;
+            });
+        });
+
+        //Previews FILES
+        folders.forEach(folder => {
+            let previewFiles = folder.dir.previews.files;
+
+            previewFiles.forEach(file => {
+                let data = fs.readFileSync(file.path, "base64")
+                file.content = data;
+            });
+        });
+
+        //Text Previews FILES
+        folders.forEach(folder => {
+            let textPreviewFiles = folder.dir.textPreviews.files;
+
+            textPreviewFiles.forEach(file => {
+                if (folder.dir.textPreviews.path != false) {
+                    let encoding = file.name.includes('.pdf') ? "base64" : "utf8";
+                    let data = fs.readFileSync(file.path, "utf8")
+                    file.content = file.name.includes('.pdf') ? data : JSON.parse(data);
+                }
+            });
+        });
+        controller = true;
+
+        if (controller == true) {
+            resolve(folders);
+        } else {
+            reject("Something went wrong while getting json content.");
+        }
+    });
+}
